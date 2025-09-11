c40cff4a59a40ecc57581b2286ab06b3864407b3
WilliamAraujo <w67543210@gmail.com>
2025-09-10 21:40:41 -0300

    Otimizando imagens e carregamentos

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      png: { quality: 70 },
+      jpeg: { quality: 70 }, 
+      webp: { quality: 75 },
+      avif: { quality: 50 },
+    }),
+  ],
 });
