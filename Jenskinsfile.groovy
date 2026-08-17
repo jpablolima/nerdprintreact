57fcb2877d5ad1518d9dec479634a033898b37d7
João Pablo <jp.pablolima@gmail.com>
2026-08-17 07:25:18 -0300

    add jenkinsfile

---
+++ Jenskinsfile.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent {
+        label 'nerdprint'
+    }
+    stages {
+        stage ("build") {
+            steps {
+            echo "building the application..."
+            sh '/home/pablo/.nvm/versions/node/v20.17.0/bin/node -v'
+            sh  "ls  -la"
+
+            }
+        }
+    }
+}
